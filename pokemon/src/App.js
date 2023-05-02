49ecb70bbcf789fe02ec400774b4b84a8da6dfa2
Ellie-Aghajani <z.aqajani@gmail.com>
2023-05-02 14:12:22 -0700

    get pokemon

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -5,6 +5,25 @@ import PokemonThumbnail from "./components/PokemonThumbnail";
 
 function App() {
   const [allPokemons,setAllPokemons] = useState([]);
+  const [loadPoke,setLoadPoke] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+  const getAllPokemons = async () =>{
+    const res = await fetch(loadPoke)
+    const data = await res.json()
+    setLoadPoke(data.next)
+   
+    function createPokemonObject(result){
+      result.forEach(async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json();
+        setAllPokemons(currentList => [...currentList,data])
+      });
+    }
+    createPokemonObject(data.results)
+    await console.log(allPokemons)
+  }
+  useEffect(()=>{
+    getAllPokemons()
+  },[])
   
   return ();
 }
